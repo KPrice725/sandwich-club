77938be88956d78f2b4dbc79c17b192a893d87e0
Kevin Mark Price <KPrice725@aol.com>
2019-04-12 07:11:49 -0400

    fix: Replace getString() with optString()
    Replace calls to JSONObject and JSONArray object's
    getString() method to their optString() method, in order
    to ensure that, in the case where a null value would
    be returned in getString(), it would instead return an
    empty string in optString().

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -29,27 +29,27 @@ public class JsonUtils {
         try {
             JSONObject jsonDetails = new JSONObject(json);
 
-            String name = jsonDetails.getString(JSON_DETAILS_KEY_NAME);
+            String name = jsonDetails.optString(JSON_DETAILS_KEY_NAME);
             JSONObject jsonName = new JSONObject(name);
 
-            String mainName = jsonName.getString(JSON_NAME_KEY_MAIN_NAME);
+            String mainName = jsonName.optString(JSON_NAME_KEY_MAIN_NAME);
 
             JSONArray jsonAlsoKnownAs = jsonName.getJSONArray(JSON_NAME_KEY_ALSO_KNOWN_AS);
             ArrayList<String> alsoKnownAs = new ArrayList<>();
             for (int i = 0; i < jsonAlsoKnownAs.length(); i++) {
-                alsoKnownAs.add(jsonAlsoKnownAs.getString(i));
+                alsoKnownAs.add(jsonAlsoKnownAs.optString(i));
             }
 
-            String placeOfOrigin = jsonDetails.getString(JSON_DETAILS_KEY_PLACE_OF_ORIGIN);
+            String placeOfOrigin = jsonDetails.optString(JSON_DETAILS_KEY_PLACE_OF_ORIGIN);
 
-            String description = jsonDetails.getString(JSON_DETAILS_KEY_DESCRIPTION);
+            String description = jsonDetails.optString(JSON_DETAILS_KEY_DESCRIPTION);
 
-            String image = jsonDetails.getString(JSON_DETAILS_KEY_IMAGE);
+            String image = jsonDetails.optString(JSON_DETAILS_KEY_IMAGE);
 
             JSONArray jsonIngredients = jsonDetails.getJSONArray(JSON_DETAILS_KEY_INGREDIENTS);
             ArrayList<String> ingredients = new ArrayList<>();
             for (int i = 0; i < jsonIngredients.length(); i++) {
-                ingredients.add(jsonIngredients.getString(i));
+                ingredients.add(jsonIngredients.optString(i));
             }
 
             return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
